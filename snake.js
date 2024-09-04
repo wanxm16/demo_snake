a2fc4d8e31fe57ed33a684ffe0de529a32aee309
wanxm16 <wanxm16@gmail.com>
2024-09-04 10:36:35 +0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -38,8 +38,30 @@ function drawRect(x, y, color) {
     ctx.fillRect(x, y, gridSize, gridSize);
 }
 
+function drawRoundedRect(x, y, width, height, radius, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.closePath();
+    ctx.fill();
+}
+
 function drawSnake() {
-    snake.forEach(segment => drawRect(segment.x, segment.y, '#FFF')); // White snake
+    snake.forEach((segment, index) => {
+        if (index === 0) {
+            drawRoundedRect(segment.x, segment.y, gridSize, gridSize, gridSize / 4, '#FFF'); // Rounded head
+        } else {
+            drawRect(segment.x, segment.y, '#FFF'); // White body
+        }
+    });
 }
 
 function drawFood() {
